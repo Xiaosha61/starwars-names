e5ca494073c38e9343f895b1c7ce00709a155d5b
Xiaosha61 <xing.xiaosha61@gmail.com>
2019-04-07 16:31:00 +0200

    chore(releasing): add tests, ciConfig and commitizen to enable semantic release

---
+++ src/index.test.js
@@ -0,0 +1,25 @@
+var expect = require('chai').expect;
+var starwars = require('./index');
+
+describe('starwars-names', function() {
+    describe('all', () => {
+        it('should be an array of strings', () => {
+            expect(starwars.all).to.satisfy(isArrayOfStrings);
+    
+            function isArrayOfStrings(array) {
+                return array.every((item) => typeof item === 'string')
+            }
+        });
+    
+        it('should contain Lucas', () => {
+            expect(starwars.all).to.include('Lucas');
+        });
+    })
+
+    describe('random', () => {
+        it('should return a random item from the starwars.all', () => {
+            var randomItem = starwars.random();
+            expect(starwars.all).to.include(randomItem);
+        });
+    })
+});
